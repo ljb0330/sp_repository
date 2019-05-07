97b3bb45bda1b5ea2df771e5602df1e84a576b62
Administrator <Administrator@eureka1>
2019-05-07 14:17:53 +0800

    sp_repository

---
+++ sp04-orderservice/src/main/java/com/tedu/sp04/order/feignclient/ItemFeignService.java
@@ -0,0 +1,28 @@
+package com.tedu.sp04.order.feignclient;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.tedu.sp01.pojo.Item;
+import com.tedu.web.util.JsonResult;
+
+/**
+ * service.getItems("abc")
+ * 向远程微服务发送请求
+ * 请求路径：http://localhost:8001/abc
+ * 
+ */
+
+@FeignClient(name="item-service", fallback = ItemFeignServiceFB.class)
+public interface ItemFeignService {
+	@GetMapping("/{orderId}")
+	JsonResult<List<Item>> getItems(@PathVariable String orderId);
+
+	@PostMapping("/decreaseNumber")
+	JsonResult decreaseNumber(@RequestBody List<Item> items);
+}
